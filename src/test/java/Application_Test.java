b2e1d1bdbe6a1bf600d1eade6ba9bbae5ca0c3a3
Swarup <swarupgulla@gmail.com>
2019-12-10 15:12:05 +0000

    New code files committed

---
+++ src/test/java/Application_Test.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+
+public class Application_Test {
+
+    @Test
+    public void test1(){
+        System.out.println("running a test");
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("running a test2");
+    }
+
+    @Test
+    public void test3(){
+        System.out.println("running a test3");
+    }
+
+
+
+}
